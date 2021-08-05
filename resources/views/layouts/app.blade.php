3791cb62794768a07af886b50703c72c8d6c1a90
endriadisaputro <endryady77@gmail.com>
2021-08-05 22:08:06 +0700

    menambahkan bootstrap & perbaiki tampilan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>{{$title}} | Skawan</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
     {{$styles}}
 </head>
 <body>
@@ -10,6 +10,6 @@
 
     {{$slot}}
 
-    <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,25 +1,30 @@
 <x-app-layout title="Task">
-	<h1 style="margin: 10px">Task Page</h1>
-	<form action="/tasks" method="post" style="margin: 10px 20px">
-		@csrf
-		<input type="text" name="list" placeholder="The name of the Task">
-		<button type="submit" >Add</button>
-	</form>
 	<div class="container">
-		<ol style="margin: 20px 30px;">
-			@foreach($tasks as $task)
-			<li style="padding: 10px">
-				{{ $task->list }}
-				<div style=" margin-top: 10px">
-					<a href="/tasks/{{$task->id}}/edit" style="color: blue">Edit</a>
+		<div class="card col-md-6 mt-4">
+			<div class="card-header">Create New Task</div>
+			<div class="card-body">
+				<form action="/tasks" method="post" class="d-flex">
+					@csrf
+					<input type="text" name="list" class="form-control me-2" placeholder="The name of the Task..">
+					<button type="submit" class="btn btn-primary">Add</button>
+				</form>
+			</div>
+		</div>
+		
+		<ul class="list-group col-md my-4">
+			@foreach($tasks as $index => $task)
+			<li style="padding: 10px" class="list-group-item d-flex justify-content-between align-items-center">
+				{{$index + 1}} - {{ $task->list }}
+				<div>
+					<a href="/tasks/{{$task->id}}/edit" class="btn btn-info btn-sm">Edit</a>
 					<form method="post" action="/tasks/{{$task->id}}" style="display: inline;">
 						@method('delete')
 						@csrf
-						<button type="submit">Delete</button>
+						<button type="submit" class="btn btn-danger btn-sm">Delete</button>
 					</form>
 				</div>
 			</li>
 			@endforeach
-		</ol>
+		</ul>
 	</div>
 </x-app-layout>
